5cc0153d9f557af0cb4e99fe5173439a3ce4d166
Koukyosyumei <koukyosyumei@hotmail.com>
2025-08-17 15:47:57 -0400

    update

--- Runwai/Gadget.lean
+++ Runwai/Gadget.lean
@@ -1,5 +1,6 @@
 import Mathlib.Algebra.Group.Units.Basic
 import Mathlib.Data.Bool.Basic
+import Mathlib.Data.List.Basic
 
 import Runwai.Typing
 
@@ -23,6 +24,38 @@ theorem length_eq_zero {α : Type} {xs : List α} :
     intro h
     simp at h
 
+lemma ne_imp_exists_diff {α: Type} {xs ys : List α}
+    (hlen : xs.length = ys.length) (hne : xs ≠ ys) :
+    ∃ i, ∃ (h : i < xs.length),
+      xs.get ⟨i, h⟩ ≠ ys.get ⟨i, by rwa [←hlen]⟩ := by
+  contrapose! hne
+  apply List.ext_get hlen
+  intro i hx hy
+  specialize hne i hx
+  simpa [←hlen] using hne
+
+lemma zip_get_ne {es : List Expr} {xs xs' : List Value} {i : Nat}
+    (h_len₁ : xs.length = es.length)
+    (h_len₂ : xs.length = xs'.length)
+    (h_i_xs : i < xs.length)
+    (h : xs.get ⟨i, h_i_xs⟩ ≠ xs'.get ⟨i, by rwa [← h_len₂]⟩) :
+    (es.zip xs).get ⟨i, by
+      rw [List.length_zip, h_len₁]
+      simp_all
+      rw[h_len₂] at h_i_xs
+      rw[h_len₁] at h_i_xs
+      exact h_i_xs⟩ ≠
+    (es.zip xs').get ⟨i, by
+      rw [List.length_zip, ← h_len₁]
+      simp_all
+      rw[h_len₂] at h_i_xs
+      rw[h_len₁] at h_i_xs
+      exact h_i_xs⟩ := by
+  intro eq
+  apply h
+  have := congrArg Prod.snd eq
+  simpa using this
+
 theorem evalprop_deterministic
   {σ : Env.ValEnv} {Δ : Env.CircuitEnv} {e : Expr} :
   ∀ {v₁ v₂}, Eval.EvalProp σ Δ e v₁ → Eval.EvalProp σ Δ e v₂ → v₁ = v₂ := by
@@ -41,22 +74,12 @@ theorem evalprop_deterministic
     rename_i es xs
     cases h₂
     case ConstArr xs' h_length' h_forall' =>
-    --set exs := es.zip xs
-     induction xs generalizing es with
-    | nil => {
-      simp_all
-    }
-    | cons xs_head xs_tail ih => {
-      simp_all
-      cases es with
-      | nil => {
-        simp_all
-      }
-      | cons es_head es_tail => {
-        simp_all
-        sorry
-      }
-    }
+    have hlen : xs.length = xs'.length := by
+      rw [← h_length, h_length']
+    by_contra hneq
+    have hneq' : ¬ xs = xs' := by sorry
+    have ⟨i, hi_lt, hi_ne⟩ := ne_imp_exists_diff hlen hneq'
+    sorry
   | Var lookup_eq =>
     cases h₂
     case Var lookup_eq' =>
