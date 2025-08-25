a2bc5e61265dc16c5dd7300710e534a96c45162d
Koukyosyumei <koukyosyumei@hotmail.com>
2025-08-25 16:17:45 -0400

    update

--- test/CommandTest.lean
+++ test/CommandTest.lean
@@ -1,16 +1,17 @@
 import Runwai.Typing
 import Runwai.Gadget
 import Runwai.Command
+import Runwai.Tactic
 
 #runwai_register circuit Assert1(2) -> {Unit| trace [i][1] == Fp 2} {let u = assert_eq(trace [i][1], Fp 2) in u}
 #runwai_check Assert1
 
-#runwai_register circuit IsZero(3) -> {Unit| trace [i][1] == if trace [i][0] == 0 then {1} else {0}} {
+#runwai_register circuit IsZero(3) -> {Unit| y == if x == Fp 0 then {Fp 1} else {Fp 0}} {
   let x = trace [i][0] in
-    let y = trace [i][0] in
+    let y = trace [i][1] in
       let inv = trace [i][2] in
-        let u₁ = assert_eq(y, (Fp 0 - inv) + 1) in
-          let u₂ = assert_eq(x*y, 0) in u₂
+        let u₁ = assert_eq(y, ((Fp 0 - x) * inv) + Fp 1) in
+          let u₂ = assert_eq(x*y, Fp 0) in u₂
 }
 
 #runwai_prove Assert1 := by {
@@ -34,5 +35,20 @@ import Runwai.Command
 
 #runwai_prove IsZero := by {
   rename_i Δ h_delta x i height hs hi ht hty hσ σ Γ
-  sorry
+  simp_all
+  rw[← h_delta] at hty
+  apply Ty.TypeJudgment.TE_LetIn
+  · apply lookup_update_self
+  · auto_judgment
+  . apply Ty.TypeJudgment.TE_LetIn
+    . apply lookup_update_self
+    · auto_judgment
+    . apply Ty.TypeJudgment.TE_LetIn
+      . apply lookup_update_self
+      · auto_judgment
+      . apply isZero_typing_soundness
+        repeat apply lookup_update_ne; simp
+        apply Ty.TypeJudgment.TE_VarEnv
+        apply lookup_update_self;
+        repeat decide
 }
