78b307d26e29ec4950f3463888602b180054f92f
Koukyosyumei <koukyosyumei@hotmail.com>
2025-09-05 15:47:37 -0400

    update

--- test/CommandTest.lean
+++ test/CommandTest.lean
@@ -15,7 +15,7 @@ import Runwai.Tactic
 }
 
 #runwai_register circuit Lookup(trace, i, 2) -> {Unit| trace [i][0] == Fp 2} {
-  let u = # Assert1 (trace [i][0] : trace [i][1]) in u
+  let u = #Assert1(trace [i][0] : trace [i][1]) in u
 }
 
 #runwai_prove Assert1 := by {
