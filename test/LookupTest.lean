ddd770cbd479fc1e8eb48a9525201f9a2810c048
Koukyosyumei <koukyosyumei@hotmail.com>
2025-09-05 09:12:17 -0400

    simplify the proof of lookup

--- test/LookupTest.lean
+++ test/LookupTest.lean
@@ -51,23 +51,12 @@ theorem lookupCircuit_correct : Ty.circuitCorrect Δ lookupCircuit 1 := by
   simp_all
   intro h
   apply Ty.TypeJudgment.TE_LetIn
-  unfold Env.lookupTy Env.updateTy
-  simp
+  rfl
   apply Ty.TypeJudgment.TE_LookUp
   rfl
   rfl
   rfl
-  let Γ' := (Env.updateTy
-    (Env.updateTy
-      (Env.updateTy [] "trace"
-        (((((Ast.Ty.field.refin (Ast.Predicate.ind (Ast.Expr.constBool true))).arr 2).refin
-                  (Ast.Predicate.ind (Ast.Expr.constBool true))).arr
-              ↑x.length).refin
-          (Ast.Predicate.ind (Ast.Expr.constBool true))))
-      "i"
-      (Ast.Ty.int.refin (Ast.Predicate.dep "v" ((Ast.Expr.var "v").binRel Ast.RelOp.lt (Ast.Expr.constZ x.length)))))
-    "u"
-    (Ast.Ty.unit.refin
+  let τ' := (Ast.Ty.unit.refin
       (List.foldl
         (fun acc y ↦
           acc.and
@@ -87,36 +76,25 @@ theorem lookupCircuit_correct : Ty.circuitCorrect Δ lookupCircuit 1 := by
             (Env.freshName ["trace", "i"] (Env.lookupCircuit Δ "assert").ident_t))
           (Env.lookupCircuit Δ "assert").ident_i (Env.freshName ["trace", "i"] (Env.lookupCircuit Δ "assert").ident_i))
         [(((Ast.Expr.var "trace").arrIdx (Ast.Expr.var "i")).arrIdx (Ast.Expr.constZ 0),
-            ((Ast.Expr.var "trace").arrIdx (Ast.Expr.var "i")).arrIdx (Ast.Expr.constZ 1))])))
-  have hs : Ty.SubtypeJudgment σ Δ Γ' (Ast.Ty.unit.refin
-    (List.foldl
-      (fun acc y ↦
-        acc.and
-          (Ast.Predicate.ind
-            (Ast.exprEq y.1
-              (Ast.renameVar
-                (Ast.renameVar y.2 (Env.lookupCircuit Δ "assert").ident_t
-                  (Env.freshName ["trace", "i"] (Env.lookupCircuit Δ "assert").ident_t) 1000)
-                (Env.lookupCircuit Δ "assert").ident_i
-                (Env.freshName ["trace", "i"] (Env.lookupCircuit Δ "assert").ident_i) 1000))))
-      (Ast.renameVarinPred
-        (Ast.renameVarinPred
-          (Ast.Predicate.ind
-            ((((Ast.Expr.var "trace").arrIdx (Ast.Expr.var "i")).arrIdx (Ast.Expr.constZ 1)).binRel Ast.RelOp.eq
-              (Ast.Expr.constF 2)))
-          (Env.lookupCircuit Δ "assert").ident_t (Env.freshName ["trace", "i"] (Env.lookupCircuit Δ "assert").ident_t))
-        (Env.lookupCircuit Δ "assert").ident_i (Env.freshName ["trace", "i"] (Env.lookupCircuit Δ "assert").ident_i))
-      [(((Ast.Expr.var "trace").arrIdx (Ast.Expr.var "i")).arrIdx (Ast.Expr.constZ 0),
-          ((Ast.Expr.var "trace").arrIdx (Ast.Expr.var "i")).arrIdx (Ast.Expr.constZ 1))]))
+            ((Ast.Expr.var "trace").arrIdx (Ast.Expr.var "i")).arrIdx (Ast.Expr.constZ 1))]))
+  let Γ' := (Env.updateTy
+    (Env.updateTy
+      (Env.updateTy [] "trace"
+        (((((Ast.Ty.field.refin (Ast.Predicate.ind (Ast.Expr.constBool true))).arr 2).refin
+                  (Ast.Predicate.ind (Ast.Expr.constBool true))).arr
+              ↑x.length).refin
+          (Ast.Predicate.ind (Ast.Expr.constBool true))))
+      "i"
+      (Ast.Ty.int.refin (Ast.Predicate.dep "v" ((Ast.Expr.var "v").binRel Ast.RelOp.lt (Ast.Expr.constZ x.length)))))
+    "u" τ')
+  have hs : Ty.SubtypeJudgment σ Δ Γ' τ'
          (Ast.Ty.unit.refin
     (Ast.Predicate.ind
       ((((Ast.Expr.var "trace").arrIdx (Ast.Expr.var "i")).arrIdx (Ast.Expr.constZ 0)).binRel Ast.RelOp.eq
         (Ast.Expr.constF 2)))) := by {
       apply Ty.SubtypeJudgment.TSub_Refine
       apply Ty.SubtypeJudgment.TSub_Refl
-      intro v
-      intro h₁
-      intro h₂
+      intro v h₁ h₂
       unfold PropSemantics.tyenvToProp at h₁
       have h₃ := h₁ "u"
       unfold Γ' at h₃
@@ -125,29 +103,16 @@ theorem lookupCircuit_correct : Ty.circuitCorrect Δ lookupCircuit 1 := by
       unfold PropSemantics.varToProp at h₃
       unfold Env.lookupTy at h₃
       simp at h₃
-      unfold Env.lookupCircuit Δ Env.freshName at h₃
+      unfold τ' Env.lookupCircuit Δ Env.freshName at h₃
       simp at h₃
       unfold Ast.renameVarinPred at h₃
       unfold Ast.renameVarinPred at h₃
-      unfold Ast.renameVar at h₃
-      simp at h₃
-      unfold Ast.renameVar at h₃
-      simp at h₃
-      unfold Ast.renameVar at h₃
-      simp at h₃
-      unfold Ast.renameVar at h₃
-      simp at h₃
-      unfold Ast.renameVar at h₃
-      simp at h₃
+      repeat unfold Ast.renameVar at h₃; simp at h₃
       unfold PropSemantics.predToProp at h₃
       obtain ⟨h₄,h₅⟩ := h₃
       unfold PropSemantics.predToProp at h₄ h₅
       unfold PropSemantics.exprToProp at h₄ h₅
-      unfold Δ
-      unfold assertCircuit
-      unfold lookupCircuit
-      unfold PropSemantics.predToProp
-      unfold PropSemantics.exprToProp
+      unfold Δ assertCircuit lookupCircuit PropSemantics.predToProp PropSemantics.exprToProp
       apply evalProp_eq_symm at h₅
       apply evalProp_eq_trans h₅ h₄
         }
@@ -156,3 +121,4 @@ theorem lookupCircuit_correct : Ty.circuitCorrect Δ lookupCircuit 1 := by
   apply Ty.TypeJudgment.TE_VarEnv
   unfold Env.lookupTy Env.updateTy
   simp
+  rfl
