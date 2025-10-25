e24606ce2513ba7d6e1ea40cbd759ad4a999430b
Koukyosyumei <koukyosyumei@hotmail.com>
2025-10-25 06:07:51 -0400

    update

--- backend/src/ast.rs
+++ backend/src/ast.rs
@@ -1,6 +1,9 @@
-use std::fs;
+use core::panic;
 use std::path::Path;
+use std::{collections::HashMap, fs};
 
+use p3_air::AirBuilder;
+use p3_field::{Field, PrimeCharacteristicRing};
 use serde::{de::value::Error, Deserialize, Serialize};
 
 /* ======== Operators ======== */
@@ -231,5 +234,206 @@ impl Expr {
         let data = fs::read_to_string(path)?;
         let expr: Expr = serde_json::from_str(&data)?;
         Ok(expr)
+    }
+}
+
+impl Expr {
+    pub fn to_ab_expr<AB: AirBuilder>(
+        &self,
+        colid_to_var_fn: &dyn Fn(bool, usize) -> AB::Var,
+        env: &HashMap<String, Expr>,
+        trace_name: &String,
+        row_index_name: &String,
+    ) -> AB::Expr
+    where
+        AB::F: Field + PrimeCharacteristicRing,
+    {
+        match &self {
+            Expr::ConstF { val } => AB::F::from_u64(*val).into(),
+            Expr::ConstN { val: _val } => panic!("something went wrong"),
+            Expr::ConstInt { val: _val } => panic!("something went wrong"),
+            Expr::ConstBool { val: _val } => panic!("something went wrong"),
+            Expr::Arr { elems } => todo!(),
+            Expr::Var { name } => env.get(name).unwrap().to_ab_expr::<AB>(
+                colid_to_var_fn,
+                env,
+                trace_name,
+                row_index_name,
+            ),
+            Expr::AssertE { lhs, rhs } => panic!("something went wrong"),
+            Expr::BoolExpr { lhs, op, rhs } => panic!("something went wrong"),
+            Expr::FieldExpr { lhs, op, rhs } => {
+                let lhs_ab = lhs.to_ab_expr::<AB>(colid_to_var_fn, env, trace_name, row_index_name);
+                let rhs_ab = rhs.to_ab_expr::<AB>(colid_to_var_fn, env, trace_name, row_index_name);
+                match op {
+                    FieldOp::Add => lhs_ab + rhs_ab,
+                    FieldOp::Sub => lhs_ab - rhs_ab,
+                    FieldOp::Mul => lhs_ab * rhs_ab,
+                    FieldOp::Div => panic!("div cannot be used within constraints"),
+                }
+            }
+            Expr::UintExpr { lhs, op, rhs } => panic!("something went wrong"),
+            Expr::SintExpr { lhs, op, rhs } => panic!("something went wrong"),
+            Expr::BinRel { lhs, op, rhs } => panic!("something went wrong"),
+            Expr::ArrIdx { arr, idx } => {
+                if let Expr::ConstN { val } = &**idx {
+                    if let Expr::ArrIdx { arr, idx } = &**arr {
+                        if let Expr::Var { name } = &**arr {
+                            if name == trace_name {
+                                if let Expr::Var { name } = &**idx {
+                                    if name == row_index_name {
+                                        colid_to_var_fn(true, (*val) as usize).into()
+                                    } else {
+                                        panic!("unresolved name: {}", name)
+                                    }
+                                } else if let Expr::UintExpr {
+                                    lhs,
+                                    op: IntOp::Add,
+                                    rhs,
+                                } = &**idx
+                                {
+                                    if let Expr::Var { name } = &**lhs {
+                                        if name == row_index_name {
+                                            if let Expr::ConstN { val } = &**rhs {
+                                                if *val == 1 {
+                                                    colid_to_var_fn(false, (*val) as usize).into()
+                                                } else {
+                                                    panic!("something went wrong")
+                                                }
+                                            } else {
+                                                panic!("something went wrong")
+                                            }
+                                        } else {
+                                            panic!("unresolved name: {}", name)
+                                        }
+                                    } else {
+                                        panic!("something went wrong")
+                                    }
+                                } else {
+                                    panic!("something went wrong")
+                                }
+                            } else {
+                                panic!("unresolved name: {}", name)
+                            }
+                        } else {
+                            todo!()
+                        }
+                    } else {
+                        todo!()
+                    }
+                } else {
+                    todo!()
+                }
+            }
+            Expr::Len { arr: _arr } => panic!("cannot use Len in the constraints"),
+            Expr::Branch { cond, th, els } => todo!(),
+            Expr::Lam { param, ty, body } => todo!(),
+            Expr::App { f, arg } => todo!(),
+            Expr::LetIn { name, val, body } => todo!(),
+            Expr::Lookup {
+                vname,
+                cname,
+                args,
+                body,
+            } => todo!(),
+            Expr::ToN { body: _body } => panic!("cannot use toN in the constraints"),
+            Expr::ToF { body: _body } => panic!("cannot use toF in the constraints"),
+            Expr::UtoS { body: _body } => panic!("cannot use UtoS in the constraints"),
+            Expr::StoU { body: _body } => panic!("cannot use StoU in the constraints"),
+        }
+    }
+}
+
+pub fn walkthrough_ast<AB: AirBuilder>(
+    builder: &mut AB,
+    env: &mut HashMap<String, Expr>,
+    expr: Expr,
+    colid_to_var_fn: &dyn Fn(bool, usize) -> AB::Var,
+    trace_name: &String,
+    row_index_name: &String,
+) where
+    AB::F: Field + PrimeCharacteristicRing,
+{
+    match expr {
+        Expr::AssertE { lhs, rhs } => {
+            let lhs_ab: AB::Expr =
+                lhs.to_ab_expr::<AB>(colid_to_var_fn, env, trace_name, row_index_name);
+            let rhs_ab: AB::Expr =
+                rhs.to_ab_expr::<AB>(colid_to_var_fn, env, trace_name, row_index_name);
+            builder.assert_eq(lhs_ab, rhs_ab);
+        }
+        Expr::Branch { cond, th, els } => {
+            if let Expr::BinRel {
+                lhs,
+                op: RelOp::Eq,
+                rhs,
+            } = &*cond
+            {
+                if let Expr::Var { name } = &**lhs {
+                    if name == row_index_name {
+                        if let Expr::ConstN { val } = &**rhs {
+                            if *val == 0 {
+                                let mut when = builder.when_first_row();
+                                walkthrough_ast(
+                                    &mut when,
+                                    env,
+                                    *th,
+                                    colid_to_var_fn,
+                                    trace_name,
+                                    row_index_name,
+                                );
+                            }
+                        }
+                    }
+                }
+            } else {
+                let cond_ab: AB::Expr =
+                    cond.to_ab_expr::<AB>(colid_to_var_fn, env, trace_name, row_index_name);
+                let mut when = builder.when(cond_ab.clone());
+                walkthrough_ast(
+                    &mut when,
+                    env,
+                    *th,
+                    colid_to_var_fn,
+                    trace_name,
+                    row_index_name,
+                );
+                let mut when = builder.when_ne(cond_ab, AB::Expr::ZERO);
+                walkthrough_ast(
+                    &mut when,
+                    env,
+                    *els,
+                    colid_to_var_fn,
+                    trace_name,
+                    row_index_name,
+                );
+            }
+        }
+        Expr::LetIn { name, val, body } => {
+            walkthrough_ast(
+                builder,
+                env,
+                *val.clone(),
+                colid_to_var_fn,
+                trace_name,
+                row_index_name,
+            );
+            env.insert(name, *val);
+            walkthrough_ast(
+                builder,
+                env,
+                *body,
+                colid_to_var_fn,
+                trace_name,
+                row_index_name,
+            );
+        }
+        Expr::Lookup {
+            vname,
+            cname,
+            args,
+            body,
+        } => todo!(),
+        _ => {}
     }
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,67 @@
-use runwai_p3::ast::Expr;
+use std::collections::HashMap;
+use std::marker::PhantomData;
+
+use p3_air::{Air, AirBuilder, AirBuilderWithPublicValues, BaseAir, PairBuilder, VirtualPairCol};
+use p3_field::{Field, PrimeCharacteristicRing};
+use p3_matrix::dense::RowMajorMatrix;
+use p3_matrix::Matrix;
+
+use runwai_p3::ast::{walkthrough_ast, Expr};
+
+#[derive(Clone)]
+pub struct RunwaiAir<F>
+where
+    F: Field,
+{
+    runwai_ast: Expr,
+    width: usize,
+    _marker: PhantomData<F>,
+}
+
+impl<F: Field> BaseAir<F> for RunwaiAir<F> {
+    fn width(&self) -> usize {
+        self.width
+    }
+}
+
+impl<AB: AirBuilderWithPublicValues + PairBuilder> Air<AB> for RunwaiAir<AB::F>
+where
+    AB::F: Field + PrimeCharacteristicRing,
+{
+    fn eval(&self, builder: &mut AB) {
+        let main = builder.main();
+        let local = main.row_slice(0).unwrap();
+        let next = main.row_slice(1).unwrap();
+
+        let load_var = |is_curr: bool, col_idx: usize| {
+            if is_curr {
+                local[col_idx].clone()
+            } else {
+                next[col_idx].clone()
+            }
+        };
+
+        let mut env = HashMap::<String, Expr>::new();
+        walkthrough_ast(
+            builder,
+            &mut env,
+            self.runwai_ast.clone(),
+            &load_var,
+            &"trace".to_string(),
+            &"i".to_string(),
+        );
+    }
+}
+
+impl<F: Field> RunwaiAir<F> {
+    pub fn new(runwai_ast: Expr, width: usize) -> Self {
+        Self {
+            runwai_ast,
+            width,
+            _marker: PhantomData,
+        }
+    }
+}
 
 fn main() {
     let expr = Expr::from_json_file("expr.json").unwrap();
