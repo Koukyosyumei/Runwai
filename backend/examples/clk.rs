2b07bbb07458a638cf33b1bb31a1a571f2d1dc48
Koukyosyumei <koukyosyumei@hotmail.com>
2025-10-25 16:26:32 -0400

    update

--- backend/examples/clk.rs
+++ backend/examples/clk.rs
@@ -5,7 +5,7 @@ use runwai_p3::air::RunwaiAir;
 use runwai_p3::ast::Expr;
 
 fn main() {
-    let expr = Expr::from_json_file("expr.json").unwrap();
+    let expr = Expr::from_json_file("examples/clk.json").unwrap();
     //println!("{:#?}", expr);
 
     let air = RunwaiAir::<KoalaBear>::new(expr, 2);
